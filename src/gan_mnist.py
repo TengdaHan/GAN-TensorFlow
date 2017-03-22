479e672ff0ff0f516bde0239e5c78649210ea0e7
Tengda Han <u5612799@anu.edu.au>
2017-03-23 00:25:30 +1100

    update and tune

--- src/gan_mnist.py
+++ src/gan_mnist.py
@@ -6,7 +6,7 @@ import os
 
 
 def conv(x, w, b, stride, name):
-    with tf.name_scope('conv'):
+    with tf.variable_scope('conv'):
         tf.summary.histogram('weight', w)
         tf.summary.histogram('biases', b)
         return tf.nn.conv2d(x,
@@ -17,7 +17,7 @@ def conv(x, w, b, stride, name):
 
 
 def deconv(x, w, b, shape, stride, name):
-    with tf.name_scope('deconv'):
+    with tf.variable_scope('deconv'):
         tf.summary.histogram('weight', w)
         tf.summary.histogram('biases', b)
         return tf.nn.conv2d_transpose(x,
@@ -26,6 +26,11 @@ def deconv(x, w, b, shape, stride, name):
                                        strides=[1, stride, stride, 1],
                                        padding='SAME',
                                        name=name) + b
+
+
+def lrelu(x, alpha=0.2):
+    with tf.variable_scope('leakyReLU'):
+        return tf.maximum(x, alpha*x)
 
 
 def read_data():
@@ -41,15 +46,16 @@ def discriminator(X, reuse=False):
             scope.reuse_variables()
 
         K = 4
-        M = 8
+        M = 2
+        N = 50
         # W1 = tf.Variable(tf.truncated_normal([3, 3, 1, K], stddev=0.1))
         # B1 = tf.Variable(tf.constant(0.1, tf.float32, [K]))
         # W2 = tf.Variable(tf.truncated_normal([3, 3, K, M], stddev=0.1))
         W2 = tf.Variable(tf.truncated_normal([3, 3, 1, M], stddev=0.1))
         B2 = tf.Variable(tf.constant(0.1, tf.float32, [M]))
-        W3 = tf.Variable(tf.truncated_normal([7*7*M, 100], stddev=0.1))
-        B3 = tf.Variable(tf.constant(0.1, tf.float32, [100]))
-        W4 = tf.Variable(tf.truncated_normal([100, 1], stddev=0.1))
+        W3 = tf.Variable(tf.truncated_normal([7*7*M, N], stddev=0.1))
+        B3 = tf.Variable(tf.constant(0.1, tf.float32, [N]))
+        W4 = tf.Variable(tf.truncated_normal([N, 1], stddev=0.1))
         B4 = tf.Variable(tf.constant(0.1, tf.float32, [1]))
 
         # conv1 = conv(X, W1, B1, stride=2, name='conv1')
@@ -62,18 +68,18 @@ def discriminator(X, reuse=False):
                                        offset=None,
                                        scale=None,
                                        variance_epsilon=1e-5)
-        flat = tf.reshape(tf.nn.relu(bn2), [-1, 7*7*M], name='flat')
-        dense = tf.nn.relu(tf.matmul(flat, W3) + B3, name='dense')
+        flat = tf.reshape(lrelu(bn2), [-1, 7*7*M], name='flat')
+        dense = lrelu(tf.matmul(flat, W3) + B3)
         logits = tf.matmul(dense, W4) + B4
         return tf.nn.sigmoid(logits), logits
 
 
 def generator(X, batch_size):
     with tf.variable_scope('generator'):
-        K = 4
-        M = 2
+        K = 16
+        M = 32
 
-        W1 = tf.Variable(tf.truncated_normal([10, 7*7*K], stddev=0.1))
+        W1 = tf.Variable(tf.truncated_normal([100, 7*7*K], stddev=0.1))
         B1 = tf.Variable(tf.constant(0.1, tf.float32, [7*7*K]))
 
         W2 = tf.Variable(tf.truncated_normal([2, 2, M, K], stddev=0.1))
@@ -84,18 +90,18 @@ def generator(X, batch_size):
         B3 = tf.Variable(tf.constant(0.1, tf.float32, [1]))
         S3 = tf.Variable(tf.constant([batch_size, 28, 28, 1], tf.int32))  # 28
 
-        XX = tf.nn.relu(tf.matmul(X, W1) + B1)
+        XX = lrelu(tf.matmul(X, W1) + B1)
         XX_reshape = tf.reshape(XX, [batch_size, 7, 7, K])
         deconv1 = deconv(XX_reshape, W2, B2, shape=S2, stride=2, name='deconv1')
-        bn1 = tf.nn.batch_normalization(deconv1, 0, 0.1, 
-        	offset=None, 
-        	scale=None, 
-        	variance_epsilon=1e-5)
+        bn1 = tf.nn.batch_normalization(deconv1, 0, 0.1,
+                                        offset=None,
+                                        scale=None,
+                                        variance_epsilon=1e-5)
         deconv2 = deconv(tf.nn.relu(bn1), W3, B3, shape=S3, stride=2, name='deconv2')
-        bn2 = tf.nn.batch_normalization(tf.nn.relu(deconv2), 0, 0.1, 
-        	offset=None, 
-        	scale=None, 
-        	variance_epsilon=1e-5)
+        bn2 = tf.nn.batch_normalization(deconv2, 0, 0.1,
+                                        offset=None,
+                                        scale=None,
+                                        variance_epsilon=1e-5)
         return tf.nn.tanh(bn2)
 
 
@@ -113,15 +119,14 @@ def train(batch_size=100):
     # Y_fake = tf.placeholder(tf.float32, [None, 2])
 
     # G
-    z = tf.placeholder(tf.float32, [None, 10])  # noise
+    z = tf.placeholder(tf.float32, [None, 100])  # noise
     tf.summary.histogram('Noise', z)
-    G = generator(z, batch_size)
-    tf.summary.image('generated image', G, 3)
+    with tf.variable_scope('GAN'):
+        G = generator(z, batch_size)
 
-    # D
-    # Input = tf.placeholder(tf.float32, [None, 28, 28, 1])
-    Y, Ylogits = discriminator(X, reuse=False)
-    Y_, Ylogits_ = discriminator(G, reuse=True)
+        Y, Ylogits = discriminator(X, reuse=False)
+        Y_, Ylogits_ = discriminator(G, reuse=True)
+    tf.summary.image('generated image', G, 3)
 
     with tf.name_scope('Prediction'):
         tf.summary.histogram('Raw', Y)
@@ -146,34 +151,39 @@ def train(batch_size=100):
     gvar = [var for var in tvar if 'generator' in var.name]
 
     with tf.name_scope('train'):
-        d_train_step = tf.train.AdamOptimizer(1e-5).minimize(d_loss, var_list=dvar)
-        g_train_step = tf.train.AdamOptimizer(1e-5).minimize(g_loss, var_list=gvar)
+        global_step = tf.Variable(0, trainable=False)
+        lr_d = tf.train.exponential_decay(1e-4, global_step, 100, 0.8, staircase=False)
+        lr_g = tf.train.exponential_decay(1e-3, global_step, 100, 0.4, staircase=False)
 
+        d_train_step = tf.train.AdamOptimizer(lr_d).minimize(d_loss, global_step=global_step, var_list=dvar)
+        g_train_step = tf.train.AdamOptimizer().minimize(g_loss, global_step=global_step, var_list=gvar)
+
+        tf.summary.scalar('lr_d', lr_d)
+        tf.summary.scalar('lr_g', lr_g)
     sess = tf.Session()
     init = tf.global_variables_initializer()
     sess.run(init)
 
     merged_summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('tmp/mnist/3')
+    writer = tf.summary.FileWriter('tmp/mnist/6')
     writer.add_graph(sess.graph)
 
-    for i in range(2500):
+    for i in range(100000):
         batch_X, _ = mnist.train.next_batch(batch_size)
         batch_X = np.reshape(batch_X, [-1, 28, 28, 1])
-        batch_noise = np.random.rand(batch_size, 10)
+        batch_noise = np.random.uniform(-1., 1., [batch_size, 100])
 
         # train G
-        g_loss_print, batch_G, _ = sess.run([g_loss, G, g_train_step],
-                                            feed_dict={X: batch_X, z: batch_noise},)
-        # # train G twice
-        # g_loss_print, batch_G, _ = sess.run([g_loss, G, g_train_step],
-        #                                     feed_dict={X: batch_X, z: batch_noise})
+        # if i % 1000 == 0:
+        g_loss_print, _ = sess.run([g_loss, g_train_step],
+                                   feed_dict={X: batch_X, z: batch_noise},)
+
         # train D
-        if i % 1000 == 0:
-        	d_loss_print, _ = sess.run([d_loss, d_train_step],
+        # if i % 1000 == 0:
+        d_loss_print, _ = sess.run([d_loss, d_train_step],
                                    feed_dict={X: batch_X, z: batch_noise})
 
-        if i % 10 == 0:
+        if i % 25 == 0:
             s = sess.run(merged_summary, feed_dict={X: batch_X, z: batch_noise})
             writer.add_summary(s, i)
             try:
@@ -181,11 +191,12 @@ def train(batch_size=100):
             except:
                 pass
 
-        # if i % 100 == 0:
-        #     try:
-        #         cv2.imwrite(os.path.join('..', 'figure', '1.png'), np.repeat(batch_G[0], 3, axis=2))
-        #     except:
-        #         pass
+        if i % 1000 == 0:
+            try:
+                batch_G = sess.run(G, feed_dict={X: batch_X, z: batch_noise})
+                cv2.imwrite(os.path.join('..', 'figure', '1.png'), (np.repeat(batch_G[0], 3, axis=2)*255))
+            except:
+                pass
 
 if __name__ == '__main__':
     train()
