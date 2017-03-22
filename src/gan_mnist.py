cd60ed8f3375761ead76f39f6acdde25ed7e3d5b
Tengda Han <htd95@126.com>
2017-03-22 14:12:54 +1100

    update code

--- src/gan_mnist.py
+++ src/gan_mnist.py
@@ -9,23 +9,23 @@ def conv(x, w, b, stride, name):
     with tf.name_scope('conv'):
         tf.summary.histogram('weight', w)
         tf.summary.histogram('biases', b)
-        return tf.nn.relu(tf.nn.conv2d(x,
-                                       filter=w,
-                                       strides=[1, stride, stride, 1],
-                                       padding='SAME',
-                                       name=name) + b)
+        return tf.nn.conv2d(x,
+                           filter=w,
+                           strides=[1, stride, stride, 1],
+                           padding='SAME',
+                           name=name) + b
 
 
 def deconv(x, w, b, shape, stride, name):
     with tf.name_scope('deconv'):
         tf.summary.histogram('weight', w)
         tf.summary.histogram('biases', b)
-        return tf.nn.relu(tf.nn.conv2d_transpose(x,
-                                                 filter=w,
-                                                 output_shape=shape,
-                                                 strides=[1, stride, stride, 1],
-                                                 padding='SAME',
-                                                 name=name) + b)
+        return tf.nn.conv2d_transpose(x,
+                                       filter=w,
+                                       output_shape=shape,
+                                       strides=[1, stride, stride, 1],
+                                       padding='SAME',
+                                       name=name) + b
 
 
 def read_data():
@@ -42,26 +42,27 @@ def discriminator(X, reuse=False):
 
         K = 4
         M = 8
-        W1 = tf.Variable(tf.truncated_normal([3, 3, 1, K], stddev=0.1))
-        B1 = tf.Variable(tf.constant(0.1, tf.float32, [K]))
-        W2 = tf.Variable(tf.truncated_normal([3, 3, K, M], stddev=0.1))
+        # W1 = tf.Variable(tf.truncated_normal([3, 3, 1, K], stddev=0.1))
+        # B1 = tf.Variable(tf.constant(0.1, tf.float32, [K]))
+        # W2 = tf.Variable(tf.truncated_normal([3, 3, K, M], stddev=0.1))
+        W2 = tf.Variable(tf.truncated_normal([3, 3, 1, M], stddev=0.1))
         B2 = tf.Variable(tf.constant(0.1, tf.float32, [M]))
         W3 = tf.Variable(tf.truncated_normal([7*7*M, 100], stddev=0.1))
         B3 = tf.Variable(tf.constant(0.1, tf.float32, [100]))
         W4 = tf.Variable(tf.truncated_normal([100, 1], stddev=0.1))
         B4 = tf.Variable(tf.constant(0.1, tf.float32, [1]))
 
-        conv1 = conv(X, W1, B1, stride=2, name='conv1')
-        bn1 = tf.nn.relu(tf.nn.batch_normalization(conv1, 0, 0.1,
-                                                   offset=None,
-                                                   scale=None,
-                                                   variance_epsilon=1e-5))
-        conv2 = conv(bn1, W2, B2, stride=2, name='conv2')
-        bn2 = tf.nn.relu(tf.nn.batch_normalization(conv2, 0, 0.1,
-                                                   offset=None,
-                                                   scale=None,
-                                                   variance_epsilon=1e-5))
-        flat = tf.reshape(bn2, [-1, 7*7*M], name='flat')
+        # conv1 = conv(X, W1, B1, stride=2, name='conv1')
+        # bn1 = tf.nn.batch_normalization(conv1, 0, 0.1,
+        #                                offset=None,
+        #                                scale=None,
+        #                                variance_epsilon=1e-5)
+        conv2 = conv(X, W2, B2, stride=2, name='conv2')
+        bn2 = tf.nn.batch_normalization(conv2, 0, 0.1,
+                                       offset=None,
+                                       scale=None,
+                                       variance_epsilon=1e-5)
+        flat = tf.reshape(tf.nn.relu(bn2), [-1, 7*7*M], name='flat')
         dense = tf.nn.relu(tf.matmul(flat, W3) + B3, name='dense')
         logits = tf.matmul(dense, W4) + B4
         return tf.nn.sigmoid(logits), logits
@@ -75,19 +76,27 @@ def generator(X, batch_size):
         W1 = tf.Variable(tf.truncated_normal([10, 7*7*K], stddev=0.1))
         B1 = tf.Variable(tf.constant(0.1, tf.float32, [7*7*K]))
 
-        W2 = tf.Variable(tf.truncated_normal([3, 3, M, K], stddev=0.1))
+        W2 = tf.Variable(tf.truncated_normal([2, 2, M, K], stddev=0.1))
         B2 = tf.Variable(tf.constant(1, tf.float32, [M]))
         S2 = tf.Variable(tf.constant([batch_size, 14, 14, M], tf.int32))  # 14
 
-        W3 = tf.Variable(tf.truncated_normal([3, 3, 1, M], stddev=0.1))
+        W3 = tf.Variable(tf.truncated_normal([2, 2, 1, M], stddev=0.1))
         B3 = tf.Variable(tf.constant(0.1, tf.float32, [1]))
         S3 = tf.Variable(tf.constant([batch_size, 28, 28, 1], tf.int32))  # 28
 
         XX = tf.nn.relu(tf.matmul(X, W1) + B1)
         XX_reshape = tf.reshape(XX, [batch_size, 7, 7, K])
         deconv1 = deconv(XX_reshape, W2, B2, shape=S2, stride=2, name='deconv1')
-        deconv2 = deconv(deconv1, W3, B3, shape=S3, stride=2, name='deconv2')
-        return deconv2
+        bn1 = tf.nn.batch_normalization(deconv1, 0, 0.1, 
+        	offset=None, 
+        	scale=None, 
+        	variance_epsilon=1e-5)
+        deconv2 = deconv(tf.nn.relu(bn1), W3, B3, shape=S3, stride=2, name='deconv2')
+        bn2 = tf.nn.batch_normalization(tf.nn.relu(deconv2), 0, 0.1, 
+        	offset=None, 
+        	scale=None, 
+        	variance_epsilon=1e-5)
+        return tf.nn.tanh(bn2)
 
 
 def train(batch_size=100):
@@ -113,6 +122,7 @@ def train(batch_size=100):
     # Input = tf.placeholder(tf.float32, [None, 28, 28, 1])
     Y, Ylogits = discriminator(X, reuse=False)
     Y_, Ylogits_ = discriminator(G, reuse=True)
+
     with tf.name_scope('Prediction'):
         tf.summary.histogram('Raw', Y)
         tf.summary.histogram('Generated', Y_)
@@ -126,10 +136,8 @@ def train(batch_size=100):
         tf.summary.scalar('d_loss', d_loss)
 
     with tf.name_scope('G_loss'):
-        g_loss_pixel = tf.reduce_mean(tf.square(tf.subtract(X, G)))
         g_loss_d = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Ylogits_, labels=tf.ones_like(Y_)))
         g_loss = g_loss_d
-        tf.summary.scalar('g_loss_pixel', g_loss_pixel)
         tf.summary.scalar('g_loss_d', g_loss_d)
         tf.summary.scalar('g_loss', g_loss)
 
@@ -138,15 +146,15 @@ def train(batch_size=100):
     gvar = [var for var in tvar if 'generator' in var.name]
 
     with tf.name_scope('train'):
-        d_train_step = tf.train.AdamOptimizer(1e-4).minimize(d_loss, var_list=dvar)
-        g_train_step = tf.train.AdamOptimizer(1e-4).minimize(g_loss, var_list=gvar)
+        d_train_step = tf.train.AdamOptimizer(1e-5).minimize(d_loss, var_list=dvar)
+        g_train_step = tf.train.AdamOptimizer(1e-5).minimize(g_loss, var_list=gvar)
 
     sess = tf.Session()
     init = tf.global_variables_initializer()
     sess.run(init)
 
     merged_summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('tmp/mnist/13')
+    writer = tf.summary.FileWriter('tmp/mnist/3')
     writer.add_graph(sess.graph)
 
     for i in range(2500):
@@ -157,11 +165,12 @@ def train(batch_size=100):
         # train G
         g_loss_print, batch_G, _ = sess.run([g_loss, G, g_train_step],
                                             feed_dict={X: batch_X, z: batch_noise},)
-        # train G twice
-        g_loss_print, batch_G, _ = sess.run([g_loss, G, g_train_step],
-                                            feed_dict={X: batch_X, z: batch_noise})
+        # # train G twice
+        # g_loss_print, batch_G, _ = sess.run([g_loss, G, g_train_step],
+        #                                     feed_dict={X: batch_X, z: batch_noise})
         # train D
-        d_loss_print, _ = sess.run([d_loss, d_train_step],
+        if i % 1000 == 0:
+        	d_loss_print, _ = sess.run([d_loss, d_train_step],
                                    feed_dict={X: batch_X, z: batch_noise})
 
         if i % 10 == 0:
